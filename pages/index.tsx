dc7b9e50c58497450aceb5eb3aa21ad78adecb5e
FibreOptics <nacalon@hotmail.com>
2022-05-18 08:19:02 +0700

    chore: eslint setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Link href="/about-vercel" />
-    </div>
+    <ChakraProvider>
+      <div className={styles.container}>
+        <Link href="/about-vercel">About</Link>
+      </div>
+    </ChakraProvider>
   );
 };
 
